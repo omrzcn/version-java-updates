f50e4e65c7b59088ebbe3188f89e1161ffec05e7
omrzcn <omrzcnn@gmail.com>
2024-03-05 16:08:19 +0300

    BigDecimalDemo methods

--- m4-optional-bigDecimal/src/main/java/com/cydeo/BigDecimalDemo.java
+++ m4-optional-bigDecimal/src/main/java/com/cydeo/BigDecimalDemo.java
@@ -1,6 +1,7 @@
 package com.cydeo;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class BigDecimalDemo {
     public static void main(String[] args) {
@@ -16,6 +17,35 @@ public class BigDecimalDemo {
         System.out.println(b1.subtract(b2));
 
         // we are gonna use after thar instead of double BigDecimal.
+
+        BigDecimal b3 =BigDecimal.ONE;
+
+        System.out.println(b1.add(BigDecimal.valueOf(15))); // 389.56
+        System.out.println(b1.multiply(new BigDecimal("15"))); // 5618.40
+
+
+        System.out.println("================scaling===================");
+
+        BigDecimal number1 = new BigDecimal("23.12");
+        System.out.println(number1.setScale(1, RoundingMode.CEILING));
+        System.out.println(number1.setScale(1,RoundingMode.FLOOR));
+        System.out.println(number1.setScale(3,RoundingMode.CEILING));
+
+
+        System.out.println("========Do not use equals(). Use compareTo()================");
+        System.out.println(new BigDecimal(2).compareTo(new BigDecimal(2)));
+        System.out.println(new BigDecimal(3).compareTo(new BigDecimal(2)));
+        System.out.println(new BigDecimal(1).compareTo(new BigDecimal(2)));
+
+
+
+
+
+
+
+
+
+
 
 
 
