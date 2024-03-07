199410c2dc410649ea79fb721718e482ecb10e02
omrzcn <omrzcnn@gmail.com>
2024-03-07 08:16:45 +0300

    java14

---
+++ m5-new-version-updates/src/main/java/com/cydeo/java14/UserTest.java
@@ -0,0 +1,14 @@
+package com.cydeo.java14;
+
+public class UserTest {
+
+    public static void main(String[] args) {
+
+        User user = new User("Mike","VA");
+        System.out.println(user.name());
+
+
+
+
+    }
+}
